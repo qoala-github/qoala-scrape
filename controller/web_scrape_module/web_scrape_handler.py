0ba1a2726d5efc362b3eca8364c7e3aa1dff2e2b
RSheran <rajindra.ramachandran@qualitapps.com>
2020-11-20 18:32:00 +0530

    partial completion with R&D

--- controller/web_scrape_module/web_scrape_handler.py
+++ controller/web_scrape_module/web_scrape_handler.py
@@ -1,12 +1,12 @@
 import sys
 import traceback
-
-from bs4 import BeautifulSoup
 import requests
 import re
 import json
 import pandas as pd
 import logging
+
+from bs4 import BeautifulSoup
 
 from app_configurations.app_settings import AppSetting
 
@@ -17,6 +17,7 @@ logger = logging.getLogger(__name__)
 class WebScrapeHandler:
 
     def fetch_site_data(self):
+        loop_ref = ""
         try:
             user_agent = app_settings.WEB_SCRAPE_USER_AGENT
             headers = {"user-agent": user_agent}
@@ -33,25 +34,20 @@ class WebScrapeHandler:
                 keyword_json = json.load(pkf)
 
             main_list = []
-            prop_match_list = []
-            keyword_list = []
-            description_list = []
-            coupon_code_list = []
-            terms_data_list = []
-            exp_data_list = []
-            sample_list = []
+            site_coupon_list = []
 
             empty_obj = {"comp_id": "", "site_name": "", "host": "", "url": "",
                          "text_type": "", "html_element": "",
                          "html_text": ""}
 
-            company_json = [company_json[0]]
+            company_list = []
+            data_count = 2  # len(company_json)
+            for csl in range(data_count):
+                company_list.append(company_json[csl])
 
-            loop_ref = ""
-
-            for c in range(len(company_json)):
+            for c in range(len(company_list)):
                 try:
-                    com_obj = company_json[c]
+                    com_obj = company_list[c]
                     print(com_obj)
                     site_urls = com_obj.get('site_url')
                     comp_id = com_obj.get('site_no')
@@ -63,6 +59,7 @@ class WebScrapeHandler:
                     for i in range(len(target_elem)):
                         target_elem_obj = target_elem[i]
                         site_url = target_elem_obj.get('site_url')
+                        site_coupon_list = []
                         print(f"{c}:{site_url}")
                         scrape_filters = target_elem_obj.get('scrape_filters')
 
@@ -72,13 +69,11 @@ class WebScrapeHandler:
                         session_ini.headers['User-Agent'] = user_agent
                         req = session_ini.get(site_url)
                         # req = requests.get(site_url, headers=headers)
-                        print(f"{c}:{req.text}")
-                        soup = BeautifulSoup(req.text, "html.parser")
+                        # print(f"{c}:{req.text}")
+                        soup = BeautifulSoup(req.content, "html5lib")
 
                         for sf in range(len(scrape_filters)):
-                            prop_attr_text_type = scrape_filters[sf].get('text_type')
                             prop_filter_list = scrape_filters[sf].get('property_filter')
-                            keyword_list = keyword_json[prop_attr_text_type].get('keyword_list')
 
                             for j in range(len(prop_filter_list)):
                                 try:
@@ -88,78 +83,72 @@ class WebScrapeHandler:
                                     parent_node = prop_obj.get('parent_node')
                                     parent_tag = parent_node.get('tag_name')
                                     parent_attr = {parent_node.get('prop'): parent_node.get('search_key')}
+                                    prop_attr_sub_list = []
                                     prop_attr_sub_list = prop_obj.get('prop_list')
-                                    attr_search_key_list = [{obj.get('prop'): obj.get('search_key')} for obj in
-                                                            prop_attr_sub_list]
-                                    tag_name_list = [obj.get('tag_name') for obj in
-                                                     prop_attr_sub_list]
+
+                                    # for description
+                                    descr_obj = [d for d in prop_attr_sub_list if d.get('text_type') == 'description']
+                                    descr_attr = {descr_obj[0].get('prop'): descr_obj[0].get('search_key')}
+                                    descr_tag_name = descr_obj[0].get('tag_name')
+
+                                    # for coupon code
+                                    coupon_code_obj = [d for d in prop_attr_sub_list if d.get('text_type') == 'code']
+                                    coupon_code_attr = {
+                                        coupon_code_obj[0].get('prop'): coupon_code_obj[0].get('search_key')}
+                                    coupon_code_tag_name = coupon_code_obj[0].get('tag_name')
+
+                                    # for terms details
+                                    terms_obj = [d for d in prop_attr_sub_list if d.get('text_type') == 'terms']
+                                    terms_attr = {terms_obj[0].get('prop'): terms_obj[0].get('search_key')}
+                                    terms_tag_name = terms_obj[0].get('tag_name')
+
+                                    # for expiry details
+                                    exp_obj = [d for d in prop_attr_sub_list if d.get('text_type') == 'expiry']
+                                    exp_attr = {exp_obj[0].get('prop'): exp_obj[0].get('search_key')}
+                                    exp_tag_name = exp_obj[0].get('tag_name')
+
                                     print(f"prop_obj:{prop_obj}")
 
                                     # get all elements from parent node
-                                    ele_list = soup.find(parent_tag, attrs=parent_attr)
-
+                                    ele_list = soup.find_all(parent_tag, attrs=parent_attr)
+                                    print(f"parent_tag(new):{parent_tag},ele_list:{ele_list}")
                                     if ele_list is not None:
-                                        children = ele_list.findChild("div", attrs=parent_attr, recursive=False)
-                                        for child in children:
-                                            sample_list.append(child)
-                                        # for el in range(len(ele_list)):
-                                        # print(ele_list[el])
-                                        """
-                                        for attr_key in range(len(attr_search_key_list)):
-                                            attr_prop = attr_search_key_list[attr_key]
-                                            html_prop = tag_name_list[attr_key]
+                                        descendant_list = []
+                                        for el in range(len(ele_list)):
+                                            el_obj = ele_list[el]
+                                            descendants_list = el_obj.descendants
+                                            descendant_list.append(descendants_list)
+                                            for ds in descendants_list:
+                                                descendant_list.append(descendants_list[ds].descendants)
+                                                #print(f"div_name:{ds.name},class:{ds.get('class', '')}")
 
-                                            if prop_attr_text_type == "description":
-                                                description = ele_list[el].find(html_prop, attr_prop)
-                                                if description is not None:
-                                                    [description_list.append(
-                                                    description and self.is_keyword_found(keyword_list,
-                                                                                          description.text) if description.text else "")]
-                                            elif prop_attr_text_type == "code":
-                                                code = ele_list[el].find(html_prop, attr_prop)
-                                                if code is not None:
-                                                    [coupon_code_list.append(code and self.is_keyword_found(keyword_list,
-                                                                                                        code.text) if code.text else "")]
-                                            elif prop_attr_text_type == "terms":
-                                                terms = ele_list[el].find(html_prop, attr_prop)
-                                                if terms is not None:
-                                                    [terms_data_list.append(terms and self.is_keyword_found(keyword_list,
-                                                                                                        terms.text) if terms.text else "")]
-                                            elif prop_attr_text_type == "expiry":
-                                                exp_data = ele_list[el].find(html_prop, attr_prop)
-                                                if exp_data is not None:
-                                                    [exp_data_list.append(self.is_keyword_found(keyword_list,
-                                                                                                exp_data.text) if exp_data.text else "")]
+                                            description = el_obj.find(descr_tag_name, descr_attr)
+                                            coupon_code = el_obj.find(coupon_code_tag_name, coupon_code_attr)
+                                            terms = el_obj.find(terms_tag_name, terms_attr)
+                                            expiry = el_obj.find(exp_tag_name, exp_attr)
 
-                                            # price = ele_list[el].find('div', attrs={'class': '_1vC4OE _2rQ-NK'})
-                                    """
-                                    """"
-                                    for m in range(len(attr_search_key_list)):
-                                        attr_search_key = attr_search_key_list[m]
-                                        ele_list = soup.find_all(tag_name, attrs=attr_search_key)
-                                        # soup.find_all("a", href=lambda href: href and "location" in href)
-                                        # soup.find_all('a', href=True, attrs={'class': '_31qSD5', '': ''}):
-                                        [main_list.append(
-                                            {"comp_id": comp_id, "site_name": site_name, "host": site_host,
-                                             "url": site_url,
-                                             "text_type": prop_attr_text_type, "html_element": str(el),
-                                             "html_text": el.text} if self.is_keyword_found(keyword_list,
-                                                                                            el.text) else empty_obj
-                                        ) for el
-                                            in ele_list]
-                                    """
+                                            descr_text = description.text if description is not None and description.text is not None else ""
+                                            cc_text = coupon_code.text if coupon_code is not None and coupon_code.text is not None else ""
+                                            terms_text = terms.text if terms is not None and terms.text is not None else ""
+                                            exp_text = expiry.text if expiry is not None and expiry.text is not None else "2020-12-31T00:00:00Z"
+
+                                            if len(cc_text) > 0 or len(descr_text) > 0:
+                                                site_coupon_list.append(
+                                                    {"description": descr_text, "code": cc_text, "terms": terms_text,
+                                                     "expiry": exp_text})
 
                                 except Exception:
                                     msg = f'WebScrapeHandler=>fetch_site_data()=>prop_loop=>{loop_ref}:{sys.exc_info()[2]}/n{traceback.format_exc()} occurred'
                                     print(msg)
                                     logger.error(msg)
+                        main_list.append(
+                            {"host": site_host, "url": site_url, "name": site_name, "coupons": site_coupon_list})
                 except Exception:
                     msg = f'WebScrapeHandler=>fetch_site_data()=>company_loop=>{loop_ref}:{sys.exc_info()[2]}/n{traceback.format_exc()} occurred'
                     print(msg)
                     logger.error(msg)
-            print(f"sample_list:{sample_list},no_of_items:{len(sample_list)}")
-            # filtered_list = [obj for obj in main_list if (obj.get('comp_id') != "" and obj.get('html_text') != "")]
-            return description_list, coupon_code_list, terms_data_list, exp_data_list  # filtered_list
+            print(f"main_list:{main_list},no_of_items:{len(main_list)}")
+            return main_list
         except Exception:
             msg = f'WebScrapeHandler=>fetch_site_data()=>before_loop=>{loop_ref}:{sys.exc_info()[2]}/n{traceback.format_exc()} occurred'
             print(msg)
@@ -180,13 +169,6 @@ class WebScrapeHandler:
             logger.error(msg)
 
     def send_promotion_data(self):
-        description_list, coupon_code_list, terms_data_list, exp_data_list = self.fetch_site_data()
-        print(f"description_list:{description_list}")
-        print("\n")
-        print(f"coupon_code_list:{coupon_code_list}")
-        print("\n")
-        print(f"terms_data_list:{terms_data_list}")
-        print("\n")
-        print(f"exp_data_list:{exp_data_list}")
-        print("\n")
-        return "success"
+        result = self.fetch_site_data()
+        res = json.dumps(result)
+        return res
